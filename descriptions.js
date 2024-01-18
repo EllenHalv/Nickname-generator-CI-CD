20f7115c766ef18ba3a33b19f8490cc36fd69bd2
Ellen Halvardsson <ellenhalvardsson@gmail.com>
2024-01-18 13:12:39 +0100

    split up randomized words into separate files

---
+++ descriptions.js
@@ -0,0 +1,6 @@
+const descriptions = [
+    'Little', 'Big', 'Sassy', 'Captain', 'Dr.', 'Insanely', 'Crazy', 'Seriously', 'Lonely', 'Old', 'Sad', 'Happy', 'Lil'
+  ];
+  
+  export default descriptions;
+  
--- script.js
+++ script.js
@@ -1,26 +1,29 @@
+// Importing descriptions and adjectives from separate files
+import descriptions from './descriptions.js';
+import adjectives from './adjectives.js';
+
 const nameInput = document.getElementById('name');
 const generatedNickname = document.getElementById('generated-nickname');
-const firstRandomWord = ['Little', 'Mr', 'Miss', 'Sir', 'Madame', 'Big', 'Bad', 'Lil', 'Feisty', 'Brave', 'Sassy', 'Captain', 'Dr.', 'Insane', 'Crazy', 'Major', 'Serious', 'Bootylicious', 'Sex-worker', 'Thirsty', 'Lonely', 'Under-aged', 'Old', 'Wrinkle-faced', 'Sad, old', 'Balding', 'Drunk', 'High-on-meth', 'Fucked-your-mom', 'Fucked-your-dad', 'Little Miss', 'Sad', 'Trump-supporting', 'Never on time', 'Horny', 'Slutty', 'Overworked', 'Glitter-Tits'];
-const secondRandomWord = ['Boo-Thang', 'SparkleTail', 'and tired', 'Ridinghood', 'Babe', 'Body-Builder', 'Baddie', 'Snake', 'Stoneface', 'Shame', 'Unusual', 'with-high-Ape-index', 'Hottie', 'Horseface', 'with an attitude', 'Party-pooper', 'with a drinking problem', 'and desperate', 'and horny for your dad', 'and horny for your mom', 'with a dildo', 'and always hungry', 'and always horny'];
-const nameEndings = ['-boi', '-licious'];
+
+// Function to capitalize the first letter of a string
+const capitalizeFirstLetter = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 const generateNameAndNickname = (event) => {
-    event.preventDefault(); // Prevent form submission behavior
+  event.preventDefault(); // Prevent form submission behavior
 
-    const fullName = nameInput.value;
+  const fullName = capitalizeFirstLetter(nameInput.value);
 
-    // Check if a random number should be added to the name
-    const shouldAddRandomNumber = Math.random() > 0.8;
+  const firstLetter = fullName.charAt(0).toUpperCase(); //TODO print a message if invalid name is entered
 
-    // If a random number should be added, select a random nameEnding
-    const randomLetters = shouldAddRandomNumber ? nameEndings[Math.floor(Math.random() * nameEndings.length)] : '';
+  const firstWord = descriptions[Math.floor(Math.random() * descriptions.length)];
+  const secondWord = adjectives[firstLetter][Math.floor(Math.random() * adjectives[firstLetter].length)];
 
-    const firstWord = firstRandomWord[Math.floor(Math.random() * firstRandomWord.length)];
-    const secondWord = secondRandomWord[Math.floor(Math.random() * secondRandomWord.length)];
-    const generatedNameAndNicknameText = `Your nickname could be: "${firstWord} ${secondWord}-${fullName}${randomLetters}".`;
+  const generatedNameAndNicknameText = `Your nickname could be: "${firstWord} ${secondWord} ${fullName}".`;
 
-    // Set the text content of the 'generated-nickname' span
-    generatedNickname.textContent = generatedNameAndNicknameText;
+  // Set the text content of the 'generated-nickname' div
+  generatedNickname.textContent = generatedNameAndNicknameText;
 };
 
 const buttonElement = document.getElementById('generate-name-and-nickname-button');
